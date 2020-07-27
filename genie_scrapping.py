b31939bf466a8757f1d3e266755f6a314c2eed03
chaspion <49503342+chaspion@users.noreply.github.com>
2020-07-28 00:16:52 +0900

    hw submit

--- genie_scrapping.py
+++ genie_scrapping.py
@@ -8,7 +8,6 @@ db = client.dbsparta
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
 data = requests.get("https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713", headers = headers)
-
 soup = BeautifulSoup(data.text, 'html.parser')
 
 allmusic = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
